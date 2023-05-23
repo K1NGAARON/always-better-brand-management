23321c6d557ff31f75fc1582103806267e95d231
K1NGAARON <gerardaaron1997@gmail.com>
2023-05-23 13:16:31 +0200

    changes LP

--- js/app.js
+++ js/app.js
@@ -7,18 +7,18 @@ $(document).scroll(function () {
     if (darkHeader) {
         if ($(this).scrollTop() > header.height()) {
             header.addClass('active');
-            headerLogo.attr('src', "/assets/logo/always-better-black.png")
+            headerLogo.attr('src', "/assets/logo/logo-black.png")
         } else {
             header.removeClass('active');
-            headerLogo.attr('src', "/assets/logo/always-better-black.png")
+            headerLogo.attr('src', "/assets/logo/logo-black.png")
         }
     } else {
         if ($(this).scrollTop() > header.height()) {
             header.addClass('active');
-            headerLogo.attr('src', "/assets/logo/always-better-black.png")
+            headerLogo.attr('src', "/assets/logo/logo-black.png")
         } else {
             header.removeClass('active');
-            headerLogo.attr('src', "/assets/logo/always-better-white.png")
+            headerLogo.attr('src', "/assets/logo/logo-white.png")
         }
     }
 });
@@ -52,3 +52,52 @@ $('.small-menu-content .nav').click(closeMenu);
 $("#menu-toggle").click(function() {
     $(".small-menu-wrapper").toggle("active");
 });
+
+// CREATE LOGO SECTION
+const target = document.querySelector('#logos-wrapper');
+const logos = [
+    {
+        img: 'https://blitz-media.io/img/clients/cfl-logo.png',
+        client: 'Cashflow For Life'
+    },
+    {
+        img: 'https://blitz-media.io/img/clients/fa-logo.png',
+        client: 'Freedom Academy'
+    },
+    {
+        img: 'https://blitz-media.io/img/clients/sunday-logo.png',
+        client: 'Sunday'
+    },
+    {
+        img: 'https://blitz-media.io/img/clients/klimatherm-logo.png',
+        client: 'Klimatherm'
+    },
+    {
+        img: 'https://blitz-media.io/img/clients/tvh.png',
+        client: 'TVH',
+    },
+    {
+        img: 'https://blitz-media.io/img/clients/lansweeper.png',
+        client: 'Lansweeper',
+    },
+    {
+        img: 'https://blitz-media.io/img/clients/kordekor-logo.png',
+        client: 'Kordekor',
+    },
+];
+
+
+function createLogos() {
+    if (target) {
+        for (let i = 0; i < 5; i++) {
+            const template = `
+                <div class="item">
+                    <img src="${logos[i].img}" alt="${logos[i].client}">
+                </div>
+            `;
+            target.insertAdjacentHTML("beforeend", template);
+        };
+    }
+};
+
+$(document).ready(createLogos);
